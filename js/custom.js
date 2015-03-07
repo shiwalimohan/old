632fa20f4b380baac6ddb923992bb90609be275d
Shiwali Mohan <shiwali.mohan@gmail.com>
2015-03-06 17:48:43 -0800

    added publications removed photo

--- js/custom.js
+++ js/custom.js
@@ -301,33 +301,9 @@ $(document).ready(function(){
 		Publications page categorization
 	++++++++++++++++++++++++++++++++++++++*/
 	
-	
-	$('div#pub-grid').mixitup({
-		layoutMode: 'list',
-		easing : 'snap',
-		transitionSpeed :600,
-		onMixEnd: function(){
-			$(".tooltips").tooltip();
-		}
-	}).on('click','div.pubmain',function(){
-		var $this = $(this), 
-			$item = $this.closest(".item");
-		
-		$item.find('div.pubdetails').slideToggle(function(){
-			$this.children("i").toggleClass('icon-collapse-alt icon-expand-alt');
-		},function(){
-			$this.children("i").toggleClass('icon-expand-alt icon-collapse-alt');
-		});
-	});
 
-	$( '#cd-dropdown' ).dropdownit( {
-		gutter : 0
-	} );
 
-	$("[name=cd-dropdown]").on("change",function(){
-		var item = this.value;		
-		$('div#pub-grid').mixitup('filter',item);
-	});
+
 
 	
 
@@ -367,6 +343,8 @@ $(document).ready(function(){
 
 
 });
+
+
 
 
 $(window).load(function(){
